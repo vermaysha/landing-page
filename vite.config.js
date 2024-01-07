9ee4a32bac571cdfba620255b2fc30a4875f7e8a
Ashary Vermaysha <vermaysha@gmail.com>
2024-01-07 21:23:32 +0700

    chore: remove build dist

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import manifestSRI from 'vite-plugin-manifest-sri';
 import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import cleanPlugin from 'vite-plugin-clean';
+
 
 export default defineConfig(({ mode }) => {
     const isProduction = mode === 'development' ? false : true;
     return {
         plugins: [
+            cleanPlugin(['public/build']),
             laravel({
                 input: [
                     'resources/js/app.js',
@@ -21,7 +24,7 @@ export default defineConfig(({ mode }) => {
             optimizeCssModules(),
             isProduction ? pluginPurgeCss({
                 content: [
-                    './resources/views/**/*.blade.php',
+                    './resources/views/landing-page/**/*.blade.php',
                     './resources/js/**/*.js',
                 ],
                 defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
